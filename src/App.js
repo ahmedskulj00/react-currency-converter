663a0907669a1280ca921a5aeb046faaa088ab9e
Ahmed <ahmedskulj10@gmail.com>
2021-05-16 11:31:09 +0200

    Initial design done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import React from "react";
+import CurrencyRow from "./CurrencyRow";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Currency Converter</h1>
+      <CurrencyRow />
+      <div className="equals">=</div>
+      <CurrencyRow />
+    </div>
+  );
 }
 
 export default App;
